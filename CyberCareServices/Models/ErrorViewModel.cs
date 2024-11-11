9e1b28f46ae0e4a5308c505e5360680796bd6ef1
Marochkin-V <mar.vlad2005@mail.ru>
2024-11-11 20:26:55 +0300

    Добавьте файлы проекта.

---
+++ CyberCareServices/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CyberCareServices.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
